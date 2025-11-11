b20e524d71efca28d5ac141a2df92a532cbf3e96
emmajb1004 <134862847+emmajb1004@users.noreply.github.com>
2025-11-11 21:44:48 +0000

    added more functionality/extended tasks

--- routes/books.js
+++ routes/books.js
@@ -7,8 +7,18 @@ router.get('/search',function(req, res, next){
 });
 
 router.get('/search-result', function (req, res, next) {
-    //searching in the database
-    res.send("You searched for: " + req.query.keyword)
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ? "
+    let search = ['%' + req.query.search_text + '%']
+    if (!req.query.search_text) {
+        return res.render("search-result.ejs", {searchResults: []});
+    }
+    db.query(sqlquery, search, (err,result) => {
+        if (err) {
+            next(err)
+        }
+        else
+            res.render("search-result.ejs", {searchResults: result})       
+    })
 });
 
 router.get('/list', function(req,res,next) {
@@ -19,9 +29,37 @@ router.get('/list', function(req,res,next) {
         if (err) {
             next(err)
         }
-        res.send(result)
+        res.render("list.ejs", {availableBooks:result})
     });
 });
+
+router.get('/addbook', function(req,res,next) {
+    res.render("addbook.ejs");
+})
+
+router.post('/bookadded', function(req,res,next) {
+    //saving data in database
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?,?)"
+    //execute sql query
+    let newrecord = [req.body.name, req.body.price]
+    db.query(sqlquery, newrecord, (err,result) => {
+        if (err) {
+            next(err)
+        }
+        else
+        res.send(`This book is added to database. Name: ${req.body.name}, Price: ${req.body.price}`);
+    })
+})
+
+router.get('/bargainbooks', function(req,res,next) {
+    let sqlquery = "SELECT * FROM books WHERE price < 20";
+    db.query(sqlquery, (err,result) => {
+        if (err) {
+            next(err)
+        }
+        res.render("bargainbooks.ejs", {availableBooks: result})
+    })
+})
 
 // Export the router object so index.js can access it
 module.exports = router
