69c258b1adc4ac16a2f947a9a72ba22639b201b7
emmajb1004 <134862847+emmajb1004@users.noreply.github.com>
2025-11-20 20:31:03 +0000

    removed mistake

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -4,15 +4,4 @@ USE berties_books;
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
 
-INSERT INTO users (username, firstName, lastName, email, hashedPassword) VALUES ('gold', 'gold', 'smiths', 'gold@smiths.com', '$2b$10$56IMMFyKK7MkJZt7zG9/Ue9jdZ5KDK5L4AEMYPpiXN9aeNXIKEELq')
-
-# Create the users table
-CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT,
-    username VARCHAR(50) UNIQUE,
-    firstName VARCHAR(50),
-    lastName VARCHAR(50),
-    email VARCHAR(100),
-    hashedPassword VARCHAR(255),
-    PRIMARY KEY (id)
-);
+INSERT INTO users (username, firstName, lastName, email, hashedPassword) VALUES ('gold', 'gold', 'smiths', 'gold@smiths.com', '$2b$10$56IMMFyKK7MkJZt7zG9/Ue9jdZ5KDK5L4AEMYPpiXN9aeNXIKEELq');
