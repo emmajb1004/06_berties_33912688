008a8df4139d20f59cca982486a70ef349d8f666
emmajb1004 <134862847+emmajb1004@users.noreply.github.com>
2025-12-04 22:56:21 +0000

    weather edit

--- routes/weather.js
+++ routes/weather.js
@@ -3,15 +3,6 @@ const express = require("express")
 const router = express.Router()
 const request = require('request')
 
-const redirectLogin = (req, res, next) => {
-        if (!req.session.userId ) {
-          res.redirect('/users/login') // redirect to the login page
-        } else { 
-            next (); // move to the next middleware function
-        } 
-    }
-
-
 //route handlers for registering users
 router.get('/now', function (req, res, next) {
             res.render("weather.ejs")
